7a6b95f9c50612e0d7ecc8b60a1d0999ad7b71be
saharjamali76 <56353997+saharjamali76@users.noreply.github.com>
2022-06-04 20:46:55 +0430

    Add files via upload

---
+++ scrpt2.js
@@ -0,0 +1,53 @@
+// rvesh 1
+function smoti(benana, orange, apple) {
+  console.log(
+    "Number of bananas =" +
+      " " +
+      benana +
+      " " +
+      "Number of oranges = " +
+      " " +
+      orange +
+      " " +
+      "Number of apples = " +
+      " " +
+      apple
+  );
+}
+smoti(5, 2, 2);
+//ravesh2
+function smoti(benana, orange, apple) {
+  console.log(
+    "Number of bananas =" +
+      " " +
+      benana +
+      " " +
+      "Number of oranges = " +
+      " " +
+      orange +
+      " " +
+      "Number of apples = " +
+      " " +
+      apple
+  );
+}
+console.log(smoti(6, 4, 2));
+
+// ravesh 3
+function smoti(benana, orange, apple) {
+  const smotiTorsh =
+    "Number of bananas =" +
+    " " +
+    benana +
+    " " +
+    "Number of oranges = " +
+    " " +
+    orange +
+    " " +
+    "Number of apples = " +
+    " " +
+    apple;
+  return smotiTorsh;
+}
+const returnSmoti = smoti(5, 4, 3);
+console.log(returnSmoti);
